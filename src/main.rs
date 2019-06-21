30af5f6325d5e02842388b92ec477623362a6da0
Lily S <lsturman@redhat.com>
2019-06-21 09:58:15 -0400

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,25 @@
 #![feature(test)]
 
 use sgx_isa::{Report, Targetinfo};
-
 extern crate test;
 
-#[cfg(not(target_env = "sgx"))]
-pub fn get_qe_ti() {
-    let _qe_ti = dcap_ql::target_info().unwrap();
-}
-
-#[cfg(not(target_env = "sgx"))]
-pub fn get_quote() {
-    let report = Report {
-        ..Default::default()
-    };  
-
-    let _quote = dcap_ql::quote(&report);
-}
-
-#[cfg(target_env = "sgx")]
-pub fn create_report() {
-    let ti = Targetinfo {
-        ..Default::default()
-    };  
-
-    let report = Report::for_target(&ti, &[0; 64]);
-}
-
+// benchmark within an enclave
 #[cfg(target_env = "sgx")]
 #[cfg(test)]
 mod sgx_tests {
     use super::*;
     use test::Bencher;
 
-
     #[bench]
     fn bench_create_report(b: &mut Bencher) {
-        b.iter(|| create_report())
+        b.iter(|| {
+            let ti = Targetinfo::default();
+            Report::for_target(&ti, &[0; 64])
+        })
     }
 }
 
+// benchmark outside of an enclave
 #[cfg(not(target_env = "sgx"))]
 #[cfg(test)]
 mod non_sgx_tests {
@@ -47,12 +27,15 @@ mod non_sgx_tests {
     use test::Bencher;
 
     #[bench]
-    fn bench_get_qe_ti(b: &mut Bencher) {
-        b.iter(|| get_qe_ti())
+    fn bench_get_qe_targetinfo(b: &mut Bencher) {
+        b.iter(|| dcap_ql::target_info().unwrap())
     }
 
     #[bench]
     fn bench_get_quote(b: &mut Bencher) {
-        b.iter(|| get_quote())
+        b.iter(|| {
+            let report = Report::default();
+            dcap_ql::quote(&report)
+        })
     }
 }
